1101b0e010b29c1cb5a8393e8655e669caa57726
정상철 <lich133@naver.com>
2022-07-11 15:34:40 +0000

    designing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `정종관 갤러리`,
+    description: `사진작가 정종관의 갤러리입니다.`,
+    author: `pnchdrnklove`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
